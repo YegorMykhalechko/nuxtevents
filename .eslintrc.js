379f7fef6e92df3c918973d0b43276e437c88b57
YegorMykhalechko <yegor.mykhalechko@gmail.com>
2022-02-19 20:02:26 +0100

    init

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false,
+  },
+  extends: ['@nuxtjs', 'plugin:nuxt/recommended', 'prettier'],
+  plugins: [],
+  // add your custom rules here
+  rules: {
+    "vue/multi-word-component-names": [
+      "error",
+        {
+          "ignores": ["index", "create", "default", "error"]
+        }
+      ],
+      "vue/this-in-template": "off",
+    
+  },
+}
